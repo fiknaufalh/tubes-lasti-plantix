c6366cc9300ad4f9481aa48f1d4e943a43a12be1
Imanuel Raditya <16521421@mahasiswa.itb.ac.id>
2023-12-09 05:52:10 +0700

    add sidebar

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,35 +1,35 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import Logo from "./Logo";
-import { useEffect } from "react";
-import { useNavigate, useSearchParams } from "react-router-dom";
+// import { useEffect } from "react";
+// import { useNavigate, useSearchParams } from "react-router-dom";
 
 export default function Sidebar(props: any) {
-    const { user, logout } = useAuth();
-    const navigate = useNavigate();
-    const [params] = useSearchParams();
-    const returnUrl = params.get("returnUrl");
+    // const navigate = useNavigate();
+    // const [params] = useSearchParams();
+    // const returnUrl = params.get("returnUrl");
 
-    useEffect(() => {
-        if (!user) navigate("/role");
-        // returnUrl ? navigate(returnUrl) : navigate("/");
-    }, [user]);
+    // useEffect(() => {
+    //     if (!user) navigate("/role");
+    //     // returnUrl ? navigate(returnUrl) : navigate("/");
+    // }, [user]);
 
     const menuItems = Array.from({ length: props.number }, (_, index) => (
         <li key={index} className="flex flex-col px-10 py-3.5">
             {
                 <button
                     type="button"
-                    className="flex text-mealshub-orange bg-white hover:bg-mealshub-orange hover:text-white font-medium rounded-2xl p-4 inline-flex group"
+                    className="flex text-plantix-light-green bg-white hover:bg-plantix-light-green hover:text-white font-medium rounded-2xl p-4 inline-flex group"
                 >
                     <div className="flex flex-col w-52">
                         <a
                             href={props[`page${index + 1}`]}
                             className="flex items-center rounded-lg group"
                         >
-                            <div className="absolute group text-mealshub-orange">
+                            <div className="absolute group text-plantix-light-green">
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
-                                    viewBox="0 0 28 33"
-                                    className="h-auto w-7 text-mealshub-orange group-hover:hidden"
+                                    viewBox="0 0 35 35"
+                                    className="h-auto w-7 text-plantix-light-green group-hover:hidden"
                                 >
                                     <path
                                         fillRule="evenodd"
@@ -40,7 +40,7 @@ export default function Sidebar(props: any) {
                                 </svg>
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
-                                    viewBox="0 0 28 33"
+                                    viewBox="0 0 35 35"
                                     className="h-auto w-7 text-white hidden group-hover:block group-hover:shadow-xl"
                                 >
                                     <path
@@ -68,7 +68,7 @@ export default function Sidebar(props: any) {
             {
                 <button
                     type="button"
-                    className="flex text-white bg-mealshub-orange font-medium rounded-2xl p-4 inline-flex"
+                    className="flex text-white bg-plantix-light-green font-medium rounded-2xl p-4 inline-flex"
                 >
                     <div className="flex w-52">
                         <a
@@ -78,7 +78,7 @@ export default function Sidebar(props: any) {
                             <div className="absolute group text-white">
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
-                                    viewBox="0 0 28 33"
+                                    viewBox="0 0 35 35"
                                     className="h-auto w-7 text-white"
                                 >
                                     <path
@@ -130,54 +130,10 @@ export default function Sidebar(props: any) {
                     </div>
                 </div>
                 <div className="flex flex-col">
-                    <ul className="flex flex-col space-y-2 font-medium text-xl text-mealshub-orange">
+                    <ul className="flex flex-col space-y-2 font-medium text-xl text-plantix-light-green">
                         {menuItems}
                     </ul>
                 </div>
-                {props.customer === true ? (
-                    <a onClick={logout} href="/role">
-                        <div className="flex flex-col px-10 py-3.5 mt-auto mb-8">
-                            <button
-                                type="button"
-                                className="flex text-mealshub-red bg-white hover:bg-mealshub-red hover:text-white font-medium rounded-2xl p-4 inline-flex group"
-                            >
-                                <div className="flex flex-col justify-center w-52">
-                                    <div className="absolute group text-mealshub-red">
-                                        <svg
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            viewBox="0 0 28 33"
-                                            className="h-auto w-7 text-mealshub-red group-hover:hidden"
-                                        >
-                                            <path
-                                                fillRule="evenodd"
-                                                clipRule="evenodd"
-                                                d="M15.5554 24.0346L17.7292 26.2083L25.4375 18.5L17.7292 10.7917L15.5554 12.9654L19.5329 16.9583H4.625V20.0417H19.5329L15.5554 24.0346ZM29.2917 4.625H7.70833C6.89058 4.625 6.10632 4.94985 5.52809 5.52809C4.94985 6.10632 4.625 6.89058 4.625 7.70833V13.875H7.70833V7.70833H29.2917V29.2917H7.70833V23.125H4.625V29.2917C4.625 30.1094 4.94985 30.8937 5.52809 31.4719C6.10632 32.0501 6.89058 32.375 7.70833 32.375H29.2917C30.9875 32.375 32.375 30.9875 32.375 29.2917V7.70833C32.375 6.0125 30.9875 4.625 29.2917 4.625Z"
-                                                fill="currentcolor"
-                                            />
-                                        </svg>
-                                        <svg
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            viewBox="0 0 28 33"
-                                            className="h-auto w-7 text-white hidden group-hover:block group-hover:shadow-xl"
-                                        >
-                                            <path
-                                                fillRule="evenodd"
-                                                clipRule="evenodd"
-                                                d="M15.5554 24.0346L17.7292 26.2083L25.4375 18.5L17.7292 10.7917L15.5554 12.9654L19.5329 16.9583H4.625V20.0417H19.5329L15.5554 24.0346ZM29.2917 4.625H7.70833C6.89058 4.625 6.10632 4.94985 5.52809 5.52809C4.94985 6.10632 4.625 6.89058 4.625 7.70833V13.875H7.70833V7.70833H29.2917V29.2917H7.70833V23.125H4.625V29.2917C4.625 30.1094 4.94985 30.8937 5.52809 31.4719C6.10632 32.0501 6.89058 32.375 7.70833 32.375H29.2917C30.9875 32.375 32.375 30.9875 32.375 29.2917V7.70833C32.375 6.0125 30.9875 4.625 29.2917 4.625Z"
-                                                fill="currentcolor"
-                                            />
-                                        </svg>
-                                    </div>
-                                    <span className="flex ms-14 whitespace-nowrap text-lg text-left">
-                                        Log Out
-                                    </span>
-                                </div>
-                            </button>
-                        </div>
-                    </a>
-                    ) : (
-                        null
-                    )}
             </div>
         </div>
     );
