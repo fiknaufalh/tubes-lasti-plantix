50759b0d94b9cb31227da786867abc2f77800ce4
fredrick03 <fredlim56@gmail.com>
2023-12-09 10:27:30 +0700

    delete venv gede bener bos

---
+++ backend/schemas.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel, DateTime, Float
+from typing import Optional
+
+class SensorPost(BaseModel):
+    loc_id : int
+    timestamp : DateTime
+    temperature : Float
+    air_humidity : Float
+    soil_humidity : Float
+    light_intensity : Float
+
+class Sensor(BaseModel):
+    sensor_id : int
+    loc_id : int
+    timestamp : DateTime
+    temperature : Float
+    air_humidity : Float
+    soil_humidity : Float
+    light_intensity : Float
