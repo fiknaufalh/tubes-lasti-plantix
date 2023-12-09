2f97322080ff40de88beab8034675546b2e3cbd4
Fikri Naufal Hamdi <18221096@std.stei.itb.ac.id>
2023-12-09 11:37:37 +0700

    feat: add weather prediction route

--- backend/route/weatherPredRoute.py
+++ backend/route/weatherPredRoute.py
@@ -8,6 +8,9 @@ import requests
 
 prediction_router = APIRouter(tags=['Weather Prediction'])
 
-@prediction_router.get("/weather-prediction")
-def get_weather_prediction(db: Session = Depends(get_db)):
-    return {"message": "Hello World"}
+@prediction_router.get("/weather-prediction/{id_location}}")
+def get_weather_prediction(db: Session = Depends(get_db), id_location: int = 1):
+    predictions = db.query(models.WeatherPrediction).filter(models.WeatherPrediction.loc_id == id_location).all()
+    if not predictions:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Prediction with id {id_location} not found")
+    return predictions
